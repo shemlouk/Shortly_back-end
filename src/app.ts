11b1c687e5872c5fdfbd8df9d42cddcb7b3aa739
samuel <shemlouk@gmail.com>
2023-02-15 18:31:46 -0300

    feat: add express server

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running");
+});
