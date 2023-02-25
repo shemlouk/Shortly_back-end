2a1612e1bf732f7205099b1d83df894b8e81b0ad
samuel <shemlouk@gmail.com>
2023-02-25 17:25:04 -0300

    feat: add routes to app server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import Ranking from "./routes/Ranking";
+import SignUp from "./routes/SignUp";
+import SignIn from "./routes/SignIn";
+import Users from "./routes/Users";
+import Urls from "./routes/Urls";
 
 dotenv.config();
 
@@ -8,6 +13,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(Ranking);
+app.use(SignUp);
+app.use(SignIn);
+app.use(Users);
+app.use(Urls);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
