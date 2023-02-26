d8008a52105758fd76a38587da4a451ae3ed08fd
samuel <shemlouk@gmail.com>
2023-02-26 07:52:37 -0300

    build: update dump.sql

--- dump.sql
+++ dump.sql
@@ -21,6 +21,39 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: sessions; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.sessions (
+    id integer NOT NULL,
+    token character varying(36) NOT NULL,
+    "userId" integer NOT NULL,
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL,
+    CONSTRAINT sessions_token_check CHECK ((length((token)::text) = 36))
+);
+
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.sessions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -54,10 +87,26 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: sessions id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions ALTER COLUMN id SET DEFAULT nextval('public.sessions_id_seq'::regclass);
+
+
+--
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.sessions VALUES (1, 'cf347022-af07-41be-aab4-1fb514eebb3d', 1, '2023-02-25 22:40:16.190402');
+INSERT INTO public.sessions VALUES (3, '625b0d84-54c7-4b4c-b00d-87d9f358f57f', 2, '2023-02-25 22:41:17.728928');
+INSERT INTO public.sessions VALUES (6, 'b594991f-652f-4e26-bcc0-2390b72c5ebb', 6, '2023-02-26 07:40:55.965832');
 
 
 --
@@ -68,13 +117,46 @@ INSERT INTO public.users VALUES (1, 'João', 'joao@driven.com.br', '$2b$10$lN6ro
 INSERT INTO public.users VALUES (2, 'Maria', 'maria@driven.com.br', '$2b$10$5Y3nCytMyvsp5NyoreA9du/519Le6PX4BTYqiBpCZ.E8kN64tpp3a', '2023-02-25 20:39:42.497871');
 INSERT INTO public.users VALUES (3, 'Vitoria', 'vitoria@driven.com.br', '$2b$10$.nvIVhMBY/BhxYJIbe4J5.FFLx3hiiAycKQ2Am.jKxkUwNKYJTUSC', '2023-02-25 20:46:49.504086');
 INSERT INTO public.users VALUES (4, 'Juaozinho', 'Juaozinho@driven.com.br', '$2b$10$Wy03lLTip/5eAOpU3jMejOkWYfKiTuYZvtYzXuWJWEvNNlYuGpjN6', '2023-02-25 20:47:01.163937');
+INSERT INTO public.users VALUES (5, 'pedrao', 'pedrao@driven.com.br', '$2b$10$ySDxUy0UfcQra9axqkJ7iebeM5Ok10dbnXmO4oHpjzL7WuCD2i9gq', '2023-02-26 07:37:00.433538');
+INSERT INTO public.users VALUES (6, 'jurema', 'jurema@driven.com.br', '$2b$10$ptHQuZX1b7IkQ.77PqZ6U.Pb8WM9Dr6EhyG/RWwPMatb/2lKSUuj.', '2023-02-26 07:40:46.878257');
+
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.sessions_id_seq', 8, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 4, true);
+SELECT pg_catalog.setval('public.users_id_seq', 6, true);
+
+
+--
+-- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sessions sessions_token_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT sessions_token_key UNIQUE (token);
+
+
+--
+-- Name: sessions sessions_userId_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT "sessions_userId_key" UNIQUE ("userId");
 
 
 --
@@ -99,6 +181,14 @@ ALTER TABLE ONLY public.users
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sessions sessions_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
 
 
 --
