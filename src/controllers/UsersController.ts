8eff54ae59036a5ee4baf0b7b2b67717feb26296
samuel <shemlouk@gmail.com>
2023-02-26 13:58:13 -0300

    feat: add getById method to users controller

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -19,6 +19,39 @@ class UserController {
       res.status(500).json(message);
     }
   }
+  async getById(req: Request, res: Response) {
+    const { userId } = res.locals.session;
+    try {
+      const { rows } = await db.query(
+        `SELECT
+            users.id,
+            users.name,
+            COALESCE(sum("visitCount"), 0) AS "visitCount",
+            CASE
+              WHEN bool_or(urls.id IS NOT NULL)
+                THEN  json_agg(
+                        json_build_object(
+                          'id', urls.id,
+                          'shortUrl', urls."shortUrl",
+                          'url', urls.url,
+                          'visitCount', urls."visitCount"
+                        )
+                      )
+                ELSE array_to_json(array[]::json[])
+            END AS "shortenedUrls"
+        FROM users
+        LEFT JOIN urls
+        ON urls."userId" = users.id
+        WHERE users.id = $1
+        GROUP BY users.id, users.name`,
+        [userId]
+      );
+      res.send(rows[0]);
+    } catch ({ message }) {
+      console.log(message);
+      res.status(500).json(message);
+    }
+  }
 }
 
 export default new UserController();
