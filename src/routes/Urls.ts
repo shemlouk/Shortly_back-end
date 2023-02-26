e9708c5cb9f7000c10a49dcccadc9e40911961f5
samuel <shemlouk@gmail.com>
2023-02-26 08:44:53 -0300

    feat: add middleware and controller to urls/shorten POST endpoint

--- src/routes/Urls.ts
+++ src/routes/Urls.ts
@@ -1,9 +1,12 @@
+import authentication from "../middlewares/authentication";
+import validateBody from "../middlewares/validateBody";
+import Urls from "../controllers/UrlsController";
 import { Router } from "express";
 
 const router = Router();
 
 router.get("/urls/open/:shortUrl");
-router.post("/urls/shorten");
+router.post("/urls/shorten", authentication, validateBody, Urls.create);
 router.delete("/urls/:id");
 router.get("/urls/:id");
 
