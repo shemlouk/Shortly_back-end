67920baf9425540be5423ca1e25d199d449d6751
samuel <shemlouk@gmail.com>
2023-02-26 09:16:31 -0300

    feat: add middleware and controller to urls/:id GET endpoint

--- src/routes/Urls.ts
+++ src/routes/Urls.ts
@@ -1,6 +1,7 @@
 import authentication from "../middlewares/authentication";
 import validateBody from "../middlewares/validateBody";
 import Urls from "../controllers/UrlsController";
+import findUrl from "../middlewares/findUrl";
 import { Router } from "express";
 
 const router = Router();
@@ -8,6 +9,6 @@ const router = Router();
 router.get("/urls/open/:shortUrl");
 router.post("/urls/shorten", authentication, validateBody, Urls.create);
 router.delete("/urls/:id");
-router.get("/urls/:id");
+router.get("/urls/:id", findUrl, Urls.getById);
 
 export default router;
